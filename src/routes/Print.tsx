be4cacc00a15921877d26fc69146619feb77f62f
Mae Morella <4561733+morellam-dev@users.noreply.github.com>
2022-04-26 12:37:40 -0400

    Hack together the rest of the print form

--- src/routes/Print.tsx
+++ src/routes/Print.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { Formik, Form, Field, useFormikContext } from 'formik';
 import DummyPrints from 'DummyPrints';
 import { useState, useEffect, ReactElement } from 'react';
@@ -7,22 +8,13 @@ import './Form.css';
 
 type Printable = typeof DummyPrints[0];
 
-enum FilamentColor {
-  Red = 'Red',
-  Blue = 'Blue',
-}
-
-interface FormValues {
-  model: number;
-  filamentColor: FilamentColor;
-}
-
+/** Get the selected model out of the form */
 const ModelProvider = ({
   render,
 }: {
   render: (model: Printable) => ReactElement;
 }) => {
-  const { model: idx } = useFormikContext().values;
+  const { modelNumber: idx } = useFormikContext<FormValues>().values;
 
   const [model, setModel] = useState<Printable>();
 
@@ -30,76 +22,133 @@ const ModelProvider = ({
   return model ? render(model) : <></>;
 };
 
-const Print = () => {
+enum FilamentColor {
+  White = 'White',
+  Pink = 'Pink',
+  Red = 'Red',
+  Blue = 'Blue',
+  Green = 'Green',
+  Yellow = 'Yellow',
+  Transparent = 'Yellow',
+  Luminsecent = 'Luminescent',
+}
+
+enum FilamentType {
+  PLA = 'PLA',
+  ABS = 'ABS',
+  PETG = 'PETG',
+  FLEX = 'FLEX',
+}
+
+interface FormValues {
+  modelNumber: number;
+  filamentColor: FilamentColor;
+  filamentType: FilamentType;
+  fillDensity: number;
+  vaseMode: boolean;
+}
+
+const FormFunction = ({ isSubmitting }: { isSubmitting: boolean }) => {};
+
+const timeEstimate = (
+  modelNumber: number,
+  fillDensity: number,
+  vaseMode: boolean
+) =>
+  (vaseMode ? 0.3 : 1) * DummyPrints[modelNumber].hoursToPrint +
+  0.1 * fillDensity;
+
+const TimeEstimator = () => {
+  const { values }: { values: FormValues } = useFormikContext<FormValues>();
+
   return (
-    <>
-      <h1>Print a Part</h1>
-      <Formik
-        initialValues={{ model: '0' }}
-        validate={(values: FormValues) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
+    <b>
+      Estimated print time:{' '}
+      {timeEstimate(
+        values.modelNumber,
+        values.fillDensity,
+        values.vaseMode
+      ).toFixed(1)}{' '}
+      hours
+    </b>
+  );
+};
+
+const Print = ({ onAddJob }: PrintProps) => (
+  <>
+    <h1>Print a Part</h1>
+    <Formik<FormValues>
+      initialValues={{ modelNumber: 0, fillDensity: 1, vaseMode: false }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(true);
+          const output = {
+            estimated: timeEstimate(
+              values.modelNumber,
+              values.fillDensity,
+              values.vaseMode
+            ),
+            ...values,
+          };
+          setSubmitting(false);
+          if (onAddJob) {
+            onAddJob(output.estimated);
           }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => {
+        return (
           <Form>
-            <div>
-              <div>
-                <label>
-                  <h3>Load model</h3>
-                  <Field as="select" name="model">
-                    {DummyPrints.map((p, i) => (
-                      <option value={i}>{p.filename}</option>
-                    ))}
-                  </Field>
-                </label>
-              </div>
-              <div>
+            <section>
+              <label>
+                <h3>Load model</h3>
+                <Field as="select" name="modelNumber">
+                  {DummyPrints.map((p, i) => (
+                    <option value={i}>{p.filename}</option>
+                  ))}
+                </Field>
+              </label>
+              <label>
+                <h3>Select filament color</h3>
                 <Field as="select" name="filamentColor">
                   {Object.entries(FilamentColor).map(([i, color]) => (
                     <option value={i}>{color}</option>
                   ))}
                 </Field>
-              </div>
-              <div>
-                <ModelProvider
-                  render={(model) => (
-                    <p>
-                      <b>Estimated print time: {model.hoursToPrint} hours</b>
-                    </p>
-                  )}
-                />
-                <button type="submit" disabled={isSubmitting}>
-                  Queue Print
-                </button>
-              </div>
-            </div>
-            <div>
+              </label>
+              <label>
+                <b>Vase mode</b> <Field type="checkbox" name="vaseMode" />
+              </label>
+              <label>
+                <b>Scaffolds</b> <Field type="checkbox" name="scaffolds" />
+              </label>
+              <label>
+                <b>Fill density</b> 1{' '}
+                <Field type="range" name="fillDensity" min="1" max="5" /> 5
+              </label>
+              <TimeEstimator />
+              <button type="submit" disabled={isSubmitting}>
+                Queue Print
+              </button>
+            </section>
+            <section>
               <h3>Model preview</h3>
               <ModelProvider
                 render={(model) => (
                   <ModelViewer image={model.image} filename={model.filename} />
                 )}
               />
-            </div>
+            </section>
           </Form>
-        )}
-      </Formik>
-    </>
-  );
+        );
+      }}
+    </Formik>
+  </>
+);
+
+type PrintProps = {
+  onAddJob: (hours: number) => void | undefined;
 };
 
 export default Print;
